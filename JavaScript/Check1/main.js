8df271762aa4f29228137533cf077ce0412bdbc0
koutatakeda <koutatakeda526@gmail.com>
2021-05-31 19:50:54 +0900

    再提出

--- JavaScript/Check1/main.js
+++ JavaScript/Check1/main.js
@@ -1,12 +1,13 @@
 let numbers = [2, 5, 12, 13, 15, 18, 22];
 //ここに答えを実装してください。↓↓↓
 for(let i = 0; i < numbers.length; i++) {
-    console.log(isEven(numbers[i]));
+   if(numbers[i] % 2 == 0) {
+       isEven(numbers[i]);
+   }
 }
+
 function isEven(num) {
-    if (num % 2 === 0)  {
     console.log(num + 'は偶数です');
-}
 }
 
 // 【要件】
@@ -17,7 +18,8 @@ class car {
     constructor(gass, num) {
         this.gass = gass;
         this.num = num;
-    }getNumGas() {
+    }
+    getNumGas() {
         console.log(`ガソリンは${this.gass}です。ナンバーは${this.num}です`);
     }
 }
